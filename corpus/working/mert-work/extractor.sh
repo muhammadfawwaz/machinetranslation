ff3f873c961d0165ba4f0c686c6990bd5ae3fd9f
Afif <muhammad.afif81@gmail.com>
2020-12-13 06:50:08 +0700

    deleting node modules and corpus dir again

--- corpus/working/mert-work/extractor.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-cd /home/aufa/corpus/working/mert-work
-/home/aufa/smt/mosesdecoder/bin/extractor --sctype BLEU --scconfig case:true  --scfile run5.scores.dat --ffile run5.features.dat -r /home/aufa/corpus/test.true.id -n run5.best100.out.gz
